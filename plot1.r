213c7c21d1bd072ce4367a7248ead2f2439b4208
Ankitha Giridhar <55665123+AnkithaGiridhar@users.noreply.github.com>
2021-03-20 13:03:53 +0530

    plot 1 code changed

--- plot1.r
+++ plot1.r
@@ -9,7 +9,6 @@ pc<-na.omit(pc)
 pc$Date <- as.Date(pc$Date, format = "%d/%m/%Y")
 pc$Time <- strptime(pc$Time, "%H:%M:%S")
 pc$Global_active_power<-as.numeric(pc$Global_active_power)
-png(file="C:/Users/palla/Desktop/ExData_Plotting1/plot1.png",
-    width=480, height=480)
+png(file="plot1.png",width=480, height=480)
 hist(pc$Global_active_power,xlim = c(0,6), ylim = c(0,1200), breaks = 12, col = "Red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
 dev.off()
